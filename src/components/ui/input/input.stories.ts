22a00da4586face8f0f0c8db4851a45557d3565c
Yana <yana.prokopenko24@mai.ru>
2023-12-20 21:12:41 +0300

    input control upd

--- src/components/ui/input/input.stories.ts
+++ src/components/ui/input/input.stories.ts
@@ -3,53 +3,50 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Input } from './'
 
 const meta = {
-
-    title: 'Components/Input',
-    component: Input,
-    tags: ['autodocs'],
-    argTypes: {
-        variant: {
-            options: ['primary', 'secondary', 'tertiary', 'link'],
-            control: { type: 'radio' },
-        },
-
-    },
-  },
+  title: 'Components/Input',
   component: Input,
   tags: ['autodocs'],
-  title: 'Components/Input',
+  argTypes: {
+    variant: {
+      options: ['primary', 'secondary', 'tertiary', 'link'],
+      control: { type: 'radio' },
+    },
+  },
 } satisfies Meta<typeof Input>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
-
-    args: {
-        variant: 'primary',
-        children: 'Primary Input',
-        disabled: true,
-        placeholder:'Input'
-    },
+  args: {
+    variant: 'primary',
+    children: 'Primary Input',
+    disabled: true,
+    placeholder: 'Input',
+    errorMessage: 'errorMessage',
+  },
 }
 export const Secondary: Story = {
-    args: {
-        variant: 'primary',
-        children: 'Secondary Input',
-        disabled: false,
-        icon:'passwordControl',
-        placeholder:'Input'
-    },
+  args: {
+    variant: 'primary',
+    children: 'Secondary Input',
+    disabled: false,
+    icon: 'passwordControl',
+    placeholder: 'Input',
+    text: 'textInput',
+    errorMessage: 'errorMessage',
+  },
 }
 
 export const Tertiary: Story = {
-    args: {
-        variant: 'tertiary',
-        children: 'Tertiary Input',
-        disabled: false,
-        loupe:'loupeSearch',
-        cross:'crossDelete',
-        placeholder:'Input search'
-    },
-
+  args: {
+    variant: 'tertiary',
+    children: 'Tertiary Input',
+    disabled: false,
+    loupe: 'loupeSearch',
+    cross: 'crossDelete',
+    placeholder: 'Input search',
+    text: 'textInput',
+    errorMessage: 'errorMessage',
+  },
 }

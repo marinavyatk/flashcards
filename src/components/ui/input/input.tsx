251cf917c51f1c55cea01b4382d478bc5267107a
Yana <yana.prokopenko24@mai.ru>
2023-12-22 19:16:01 +0300

    input control upd

--- src/components/ui/input/input.tsx
+++ src/components/ui/input/input.tsx
@@ -1,35 +1,53 @@
 import { ComponentPropsWithoutRef, ElementType } from 'react'
 
 import s from './input.module.scss'
+import { UseFormRegister } from 'react-hook-form'
 
-export type InputProps<T extends ElementType ='input'> = {
-    as?:T
-    variant?: 'primary' | 'tertiary'
-    className?: string
-    placeholder:string
-    icon?:string
-    loupe?:string
-    cross?:string
-
-
+export type InputProps<T extends ElementType = 'input'> = {
+  as?: T
+  variant?: 'primary' | 'tertiary'
+  className?: string
+  placeholder: string
+  icon?: string
+  loupe?: string
+  cross?: string
+  textInput?: string
+  errorMessage?: string
+  controlName: string
+  register: UseFormRegister<any>
 } & ComponentPropsWithoutRef<T>
 
 export const Input = <T extends ElementType = 'input'>(
-    props: InputProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof InputProps<T>>
+  props: InputProps<T> & Omit<ComponentPropsWithoutRef<T>, keyof InputProps<T>>
 ) => {
-    const { variant = '',className,icon='',loupe='', as: Component = 'input',cross='',placeholder=''} = props
+  const {
+    variant = '',
+    className,
+    icon = '',
+    loupe = '',
+    as: Component = 'input',
+    cross = '',
+    placeholder = '',
+    textInput = '',
+    errorMessage = '',
+    controlName = '',
+    register,
+  } = props
 
-    return (
-        <div>
-            <div className={s.textInput}>Input</div>
-            <div className={s.inputClass}>
-                <Component className={`${s[variant]}  ${className} `} placeholder={placeholder}  />
-                {icon && <div className={s.passwordControl}></div>}
-                {loupe && <div className={s.loupeSearch}></div>}
-                {cross && <div className={s.crossDelete}></div>}
-
-            </div>
-        </div>
-
-    )
+  return (
+    <div>
+      <div className={s.textInput}>{textInput}</div>
+      <div className={s.inputClass}>
+        <Component
+          className={`${s[variant]}  ${className} `}
+          placeholder={placeholder}
+          {...register(controlName)}
+        />
+        {<div className={s.errorMessage}>{errorMessage}</div>}
+        {icon && <div className={s.passwordControl}></div>}
+        {loupe && <div className={s.loupeSearch}></div>}
+        {cross && <div className={s.crossDelete}></div>}
+      </div>
+    </div>
+  )
 }

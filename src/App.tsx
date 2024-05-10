d33f99a66745779d7a5257e28d60b60e21b143e7
Marina Vyatkina <marinavyatk74@mail.ru>
2024-05-10 17:44:25 +0400

    Added polymorphes textfield

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
+import TextareaAutosize from 'react-textarea-autosize'
+
 import OutIcon from '@/assets/svg/icon-out.svg?react'
 import { Button } from '@/components/ui/button'
 import { CheckboxComponent } from '@/components/ui/checkbox'
+import { TextField } from '@/components/ui/textField'
 
 export function App() {
   return (
@@ -14,6 +17,26 @@ export function App() {
       <Button as={'a'}>
         <OutIcon /> Кнопочка
       </Button>
+      <div style={{ width: '300px' }}>
+        <TextField />
+        <TextField label={'email'} placeholder={'email address'} />
+        <TextField variant={'password'} />
+        <TextField
+          errorMessage={'eeeeerrrrrrrrooor!!!!'}
+          label={'error input'}
+          variant={'password'}
+        />
+        <TextField variant={'search'} />
+        <TextField disabled placeholder={'disabled'} variant={'search'} />
+        <TextField disabled placeholder={'disabled'} variant={'password'} />
+        <TextField as={'textarea'} placeholder={'textarea'} />
+        <TextField
+          as={TextareaAutosize}
+          placeholder={
+            'textarea from special library textarea from special librarytextarea from special library'
+          }
+        />
+      </div>
     </div>
   )
 }

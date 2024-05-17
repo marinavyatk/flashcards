5e9d6eef7cf17670c7a033939917d6175eaf30d0
Marina Vyatkina <marinavyatk74@mail.ru>
2024-05-17 22:59:26 +0400

    Set up store & added basic decks query

---
+++ src/common/router.tsx
@@ -0,0 +1,40 @@
+import {
+  Navigate,
+  Outlet,
+  RouteObject,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom'
+
+import { DecksPage } from '@/pages/deckPage/deckPage'
+
+const publicRoutes: RouteObject[] = [
+  {
+    element: <div>login</div>,
+    path: '/login',
+  },
+]
+
+const privateRoutes: RouteObject[] = [
+  {
+    element: <DecksPage />,
+    path: '/',
+  },
+]
+
+const router = createBrowserRouter([
+  {
+    children: privateRoutes,
+    element: <PrivateRoutes />,
+  },
+  ...publicRoutes,
+])
+
+function PrivateRoutes() {
+  const isAuthenticated = true
+
+  return isAuthenticated ? <Outlet /> : <Navigate to={'/login'} />
+}
+export function Router() {
+  return <RouterProvider router={router} />
+}
